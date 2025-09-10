052bcf9fdb4f8f9f5437249953ee17fac6c4453e
Alessio <lexawrld02@gmail.com>
2025-09-10 12:05:25 +0200

    bello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -822,7 +822,7 @@ export default function HomePage() {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: data,
-            keepalive: true // Importante per richieste durante unload 5
+            keepalive: true // Importante per richieste durante unload 55
           });
         } catch (e) {
           // Fetch fallito
